62e6828a00b8217ab0b2fdf77c7cf4b83744bf6f
RutananaArnold <arnoldrutanana@gmail.com>
2022-06-29 08:40:24 +0300

    TRANSACTION FUNCYIONALITY COMPLETE

--- lib/controllers/get_balance_controller.dart
+++ lib/controllers/get_balance_controller.dart
@@ -1,31 +1,53 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
 
+import 'dart:ffi';
+
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 import 'dart:convert';
 
-import 'package:transaction_app/constants.dart';
-
 class BalanceController extends GetxController {
-  late String balance = "0";
+  int balance = 0;
+  int user2balance = 0;
+
+  _setHeaders() => {
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        "Access-Control-Allow-Origin": "*",
+      };
 
   updateBalance() async {
-    var jsonResponse;
-
     var response = await http.get(
-      Uri.parse("http://" + apiUrl + "/api/user/fetch/1"),
-      // headers: _setHeaders(),
+      Uri.parse("https://ve8yx5.deta.dev/api/user/fetch/1"),
+      headers: _setHeaders(),
     );
     if (response.statusCode == 200) {
-      jsonResponse = json.decode(response.body);
-      if (jsonResponse != null) {
-        
-      }
+      var jsonResponse = json.decode(response.body);
+      print(jsonResponse);
+      balance = jsonResponse;
+      update();
     }
   }
 
- String getBalance() {
+  updateUser2Balance() async {
+    var response = await http.get(
+      Uri.parse("https://ve8yx5.deta.dev/api/user/fetch/2"),
+      headers: _setHeaders(),
+    );
+    if (response.statusCode == 200) {
+      var jsonResponse = json.decode(response.body);
+      print(jsonResponse);
+      user2balance = jsonResponse;
+      update();
+    }
+  }
+
+  int getBalance() {
     return balance;
+  }
+
+  int getUser2Balance() {
+    return user2balance;
   }
 }
--- lib/screens/user2.dart
+++ lib/screens/user2.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:transaction_app/controllers/get_balance_controller.dart';
+import 'package:transaction_app/widgets/wallet_element.dart';
+import 'package:http/http.dart' as http;
+import 'package:get/get.dart';
 
 class User2 extends StatefulWidget {
   User2({Key? key}) : super(key: key);
@@ -8,8 +15,361 @@ class User2 extends StatefulWidget {
 }
 
 class _User2State extends State<User2> {
+  final topupController = TextEditingController();
+  final withdrawController = TextEditingController();
+  final sendController = TextEditingController();
+  final BalanceController balanceController = Get.put(BalanceController());
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: size.height * 0.3,
+            ),
+            const Text(
+              "BALANCE:",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            GetBuilder<BalanceController>(
+              builder: (_) => Text(
+                balanceController.user2balance.toString(),
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+            // wallet UI starts here
+            Container(
+                margin: const EdgeInsets.all(10),
+                height: size.height / 6,
+                width: size.width,
+                decoration: BoxDecoration(
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 3.0, // soften the shadow
+                      spreadRadius: 0.0, //extend the shadow
+                      offset: Offset(
+                        0.0, // Move to right 10  horizontally
+                        0.0, // Move to bottom 10 Vertically
+                      ),
+                    )
+                  ],
+                  color: Theme.of(context).backgroundColor,
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20)),
+                ),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          // topup option
+                          WalletElement(
+                              icon: Icons.add_box_rounded,
+                              label: "Topup",
+                              tap: () {
+                                showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      final size = MediaQuery.of(context).size;
+                                      return CupertinoAlertDialog(
+                                        content: StatefulBuilder(
+                                          builder: (context, setState) {
+                                            return SingleChildScrollView(
+                                              child: Column(
+                                                mainAxisSize: MainAxisSize.min,
+                                                children: [
+                                                  const SizedBox(
+                                                    height: 15,
+                                                  ),
+                                                  const Text(
+                                                      "Enter amount to top up"),
+                                                  const SizedBox(
+                                                    height: 10,
+                                                  ),
+                                                  Material(
+                                                    child: TextFormField(
+                                                      controller:
+                                                          topupController,
+                                                      keyboardType:
+                                                          TextInputType.number,
+                                                    ),
+                                                  ),
+                                                  const SizedBox(
+                                                    height: 10,
+                                                  ),
+                                                  ElevatedButton(
+                                                      onPressed: () {
+                                                        balanceController
+                                                            .updateBalance();
+                                                        topup_request(int.parse(
+                                                            topupController
+                                                                .text));
+                                                      },
+                                                      child:
+                                                          const Text("TOPUP"))
+                                                ],
+                                              ),
+                                            );
+                                          },
+                                        ),
+                                      );
+                                    });
+                              }),
+
+                          // withdraw option
+                          WalletElement(
+                              icon: Icons.file_download_outlined,
+                              label: "Withdraw",
+                              tap: () {
+                                showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      final size = MediaQuery.of(context).size;
+                                      return CupertinoAlertDialog(
+                                        content: StatefulBuilder(
+                                          builder: (context, setState) {
+                                            return SingleChildScrollView(
+                                              child: Column(
+                                                mainAxisSize: MainAxisSize.min,
+                                                children: [
+                                                  const SizedBox(
+                                                    height: 15,
+                                                  ),
+                                                  const Text(
+                                                      "Enter amount to withdraw"),
+                                                  const SizedBox(
+                                                    height: 10,
+                                                  ),
+                                                  Material(
+                                                    child: TextFormField(
+                                                      controller:
+                                                          withdrawController,
+                                                      keyboardType:
+                                                          TextInputType.number,
+                                                    ),
+                                                  ),
+                                                  const SizedBox(
+                                                    height: 10,
+                                                  ),
+                                                  ElevatedButton(
+                                                      onPressed: () {
+                                                        balanceController
+                                                            .updateBalance();
+                                                        withdraw_request(
+                                                            int.parse(
+                                                                withdrawController
+                                                                    .text));
+                                                      },
+                                                      child: const Text(
+                                                          "WITHDRAW"))
+                                                ],
+                                              ),
+                                            );
+                                          },
+                                        ),
+                                      );
+                                    });
+                              }),
+
+                          //send option
+                          WalletElement(
+                              icon: Icons.send_rounded,
+                              label: "Send",
+                              tap: () {
+                                showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      final size = MediaQuery.of(context).size;
+                                      return CupertinoAlertDialog(
+                                        content: StatefulBuilder(
+                                          builder: (context, setState) {
+                                            return SingleChildScrollView(
+                                              child: Column(
+                                                mainAxisSize: MainAxisSize.min,
+                                                children: [
+                                                  const SizedBox(
+                                                    height: 15,
+                                                  ),
+                                                  const Text(
+                                                      "Enter amount to send"),
+                                                  const SizedBox(
+                                                    height: 10,
+                                                  ),
+                                                  Material(
+                                                    child: TextFormField(
+                                                      controller:
+                                                          sendController,
+                                                      keyboardType:
+                                                          TextInputType.number,
+                                                    ),
+                                                  ),
+                                                  const SizedBox(
+                                                    height: 10,
+                                                  ),
+                                                  ElevatedButton(
+                                                      onPressed: () {
+                                                        balanceController
+                                                            .updateBalance();
+                                                        send_request(int.parse(
+                                                            sendController
+                                                                .text));
+                                                      },
+                                                      child: const Text("SEND"))
+                                                ],
+                                              ),
+                                            );
+                                          },
+                                        ),
+                                      );
+                                    });
+                              })
+                        ],
+                      ),
+                    )
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // transaction logic
+
+  _setHeaders() => {
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        "Access-Control-Allow-Origin": "*",
+      };
+
+  topup_request(int amount) async {
+    Map data = {
+      "balance": amount,
+    };
+    print(data);
+
+    var jsonResponse;
+
+    var response = await http.put(
+      Uri.parse("https://ve8yx5.deta.dev/api/user/topup/2"),
+      body: jsonEncode(data),
+      headers: _setHeaders(),
+    );
+    if (response.statusCode == 200) {
+      jsonResponse = json.decode(response.body);
+      print(jsonResponse);
+      if (jsonResponse != null) {
+        final snackBar = SnackBar(
+          content: Text('You have topped up ${topupController.text}'),
+        );
+
+        // Find the ScaffoldMessenger in the widget tree
+        // and use it to show a SnackBar.
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        Navigator.pop(context);
+      }
+    } else {
+      jsonResponse = json.decode(response.body);
+    }
+  }
+
+//withdraw logic
+  withdraw_request(int amount) async {
+    Map data = {
+      "balance": amount,
+    };
+    print(data);
+
+    var jsonResponse;
+
+    var response = await http.put(
+      Uri.parse("https://ve8yx5.deta.dev/api/user/withdraw/2"),
+      body: jsonEncode(data),
+      headers: _setHeaders(),
+    );
+    if (response.statusCode == 200) {
+      jsonResponse = json.decode(response.body);
+      print(jsonResponse);
+      if (jsonResponse == null) {
+        final snackBar = SnackBar(
+          content: Text('You have withdrawn ${withdrawController.text}'),
+        );
+
+        // Find the ScaffoldMessenger in the widget tree
+        // and use it to show a SnackBar.
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        Navigator.pop(context);
+      } else {
+        final snackBar = SnackBar(
+          content: Text(jsonResponse),
+        );
+
+        // Find the ScaffoldMessenger in the widget tree
+        // and use it to show a SnackBar.
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        Navigator.pop(context);
+      }
+    } else {
+      jsonResponse = json.decode(response.body);
+      final snackBar = SnackBar(
+        content: Text(jsonResponse),
+      );
+
+      // Find the ScaffoldMessenger in the widget tree
+      // and use it to show a SnackBar.
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      Navigator.pop(context);
+    }
+  }
+
+//sending logic
+  send_request(int amount) async {
+    Map data = {
+      "balance": amount,
+    };
+    print(data);
+
+    var jsonResponse;
+
+    var response = await http.put(
+      Uri.parse("https://ve8yx5.deta.dev/api/user/send/2/1"),
+      body: jsonEncode(data),
+      headers: _setHeaders(),
+    );
+    if (response.statusCode == 200) {
+      jsonResponse = json.decode(response.body);
+      print(jsonResponse);
+      if (jsonResponse != null) {
+        final snackBar = SnackBar(
+          content: Text(jsonResponse),
+        );
+
+        // Find the ScaffoldMessenger in the widget tree
+        // and use it to show a SnackBar.
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        Navigator.pop(context);
+      }
+    } else {
+      jsonResponse = json.decode(response.body);
+      final snackBar = SnackBar(
+        content: Text(jsonResponse),
+      );
+
+      // Find the ScaffoldMessenger in the widget tree
+      // and use it to show a SnackBar.
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      Navigator.pop(context);
+    }
   }
 }
